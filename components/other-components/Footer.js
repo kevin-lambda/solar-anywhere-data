57bf2a7a5227abc36e7646b7fba58e06ad4f9b83
Kevin Lam <mtn013209@gmail.com>
2023-05-17 01:32:24 -0700

    pre deploy

--- components/other-components/Footer.js
+++ components/other-components/Footer.js
@@ -5,15 +5,9 @@ function Footer() {
     <footer className="footer hero-bg">
       <div className="content has-text-centered">
         <p>
-          <strong>Bulma</strong> by{" "}
-          <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is
-          licensed
-          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The
-          website content is licensed{" "}
-          <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
-            CC BY NC SA 4.0
-          </a>
-          .
+          Made by <a href="https://kevin-lambda.github.io/">Kevin L</a>. The
+          source code is licensed{" "}
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
         </p>
       </div>
     </footer>
