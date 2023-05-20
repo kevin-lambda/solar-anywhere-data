ba4350e5cad3cf46c499a0b833b72b6fdedfac4e
Kevin Lam <mtn013209@gmail.com>
2023-05-19 17:37:15 -0700

    changes req

--- components/other-components/Footer.js
+++ components/other-components/Footer.js
@@ -8,6 +8,8 @@ function Footer() {
           Made by <a href="https://kevin-lambda.github.io/">Kevin L</a>. The
           source code is licensed{" "}
           <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+          SolarAnywhere® is a registered trademark of Clean Power Research, used
+          with permission.
         </p>
       </div>
     </footer>
